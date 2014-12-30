138195e2e36516f2e4d9561488a8d8912d29010b
John Jung <jej@uchicago.edu>
2014-12-30 15:16:24 -0600

    Brought in UChicago/ILS/Logic/Holds.php

--- module/UChicago/config/module.config.php
+++ module/UChicago/config/module.config.php
@@ -14,6 +14,38 @@ $config = array(
             'my-research' => 'UChicago\Controller\MyResearchController',
         ),
     ),
+    'service_manager' => array(
+        'allow_override' => true,
+        'factories' => array(
+            'UChicago\ILSHoldLogic' => function ($sm) {
+                return new \UChicago\ILS\Logic\Holds(
+                    $sm->get('VuFind\ILSAuthenticator'), $sm->get('VuFind\ILSConnection'),
+                    $sm->get('VuFind\HMAC'), $sm->get('VuFind\Config')->get('config')
+                );
+            },
+        ),
+    ),//service_manager
+    'vufind' => array(
+        'plugin_managers' => array(
+            'recorddriver' => array(
+                'factories' => array(
+                    'solrmarc' => function ($sm) {
+                        $driver = new \UChicago\RecordDriver\SolrMarcPhoenix(
+                            $sm->getServiceLocator()->get('VuFind\Config')->get('config'),
+                            null,
+                            $sm->getServiceLocator()->get('VuFind\Config')->get('searches')
+                        );
+                        $driver->attachILS(
+                            $sm->getServiceLocator()->get('VuFind\ILSConnection'),
+                            $sm->getServiceLocator()->get('UChicago\ILSHoldLogic'),
+                            $sm->getServiceLocator()->get('VuFind\ILSTitleHoldLogic')
+                        );
+                        return $driver;
+                    },
+                ),
+            ),
+        ),
+    ),
 );
 
 return $config;
