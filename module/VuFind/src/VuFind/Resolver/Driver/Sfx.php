ef688fd0922ebfe6110752c2fd1a910ca11df16a
John Jung <jej@uchicago.edu>
2015-04-16 10:23:38 -0500

    Bug # 14602. SFX embargo data now appears.

--- module/VuFind/src/VuFind/Resolver/Driver/Sfx.php
+++ module/VuFind/src/VuFind/Resolver/Driver/Sfx.php
@@ -79,7 +79,7 @@ class Sfx implements DriverInterface
     public function fetchLinks($openURL)
     {
         // Make the call to SFX and load results
-        $url = $this->baseUrl . 
+        $url = $this->baseUrl .
             '?sfx.response_type=multi_obj_detailed_xml&svc.fulltext=yes&' . $openURL;
         $feed = $this->httpClient->setUri($url)->send()->getBody();
         return $feed;
@@ -97,7 +97,7 @@ class Sfx implements DriverInterface
      */
     public function parseLinks($xmlstr)
     {
-        $records = array(); // array to return
+        $records = []; // array to return
         try {
             $xml = new \SimpleXmlElement($xmlstr);
         } catch (\Exception $e) {
@@ -107,12 +107,13 @@ class Sfx implements DriverInterface
         $root = $xml->xpath("//ctx_obj_targets");
         $xml = $root[0];
         foreach ($xml->children() as $target) {
-            $record = array();
+            $record = [];
             $record['title'] = (string)$target->target_public_name;
             $record['href'] = (string)$target->target_url;
             $record['service_type'] = (string)$target->service_type;
             $record['coverage'] = (string)$target->coverage->coverage_text
-                ->threshold_text->coverage_statement;
+                ->threshold_text->coverage_statement . ' ' .
+                (string)$target->coverage->coverage_text->embargo_text->embargo_statement;
             array_push($records, $record);
         }
         return $records;
