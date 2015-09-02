205098eadfa516232c7c6f9908a9a0c6306f5f7c
John Jung <jej@uchicago.edu>
2015-09-02 12:57:24 -0500

    Bug # 17314: Single search results should jump directly to the record.

--- module/UChicago/src/UChicago/Controller/SearchController.php
+++ module/UChicago/src/UChicago/Controller/SearchController.php
@@ -26,11 +26,155 @@
  * @link     http://vufind.org   Main Site
  */
 namespace UChicago\Controller;
+use Zend\Stdlib\Parameters;
 
 class SearchController extends \VuFind\Controller\SearchController
 {
     public function homeAction()
     {
         return $this->forwardTo('Search', 'Advanced');
+    }
+
+    /**
+     * Send search results to results view
+     *
+     * @return \Zend\View\Model\ViewModel
+     */
+    public function resultsAction()
+    {
+        $view = $this->createViewModel();
+
+        // Handle saved search requests:
+        $savedId = $this->params()->fromQuery('saved', false);
+        if ($savedId !== false) {
+            return $this->redirectToSavedSearch($savedId);
+        }
+
+        $results = $this->getResultsManager()->get($this->searchClassId);
+        $params = $results->getParams();
+        $params->recommendationsEnabled($this->getActiveRecommendationSettings());
+
+        // Send both GET and POST variables to search class:
+        $params->initFromRequest(
+            new Parameters(
+                $this->getRequest()->getQuery()->toArray()
+                + $this->getRequest()->getPost()->toArray()
+            )
+        );
+
+        // Make parameters available to the view:
+        $view->params = $params;
+
+        // Attempt to perform the search; if there is a problem, inspect any Solr
+        // exceptions to see if we should communicate to the user about them.
+        try {
+            // Explicitly execute search within controller -- this allows us to
+            // catch exceptions more reliably:
+            $results->performAndProcessSearch();
+
+            if ($results->getResultTotal() == 1) {
+                $jumpto = 1;
+            } else {
+                $jumpto = null;
+            }
+            
+            // If a "jumpto" parameter is set, deal with that now:
+            if ($jump = $this->processJumpTo($results, $jumpto)) {
+                return $jump;
+            }
+
+            // Send results to the view and remember the current URL as the last
+            // search.
+            $view->results = $results;
+            $this->rememberSearch($results);
+
+            // Add to search history:
+            if ($this->saveToHistory) {
+                $user = $this->getUser();
+                $sessId = $this->getServiceLocator()->get('VuFind\SessionManager')
+                    ->getId();
+                $history = $this->getTable('Search');
+                $history->saveSearch(
+                    $this->getResultsManager(), $results, $sessId,
+                    $history->getSearches(
+                        $sessId, isset($user->id) ? $user->id : null
+                    )
+                );
+            }
+
+            // Set up results scroller:
+            if ($this->resultScrollerActive()) {
+                $this->resultScroller()->init($results);
+            }
+        } catch (\VuFindSearch\Backend\Exception\BackendException $e) {
+            if ($e->hasTag('VuFind\Search\ParserError')) {
+                // If it's a parse error or the user specified an invalid field, we
+                // should display an appropriate message:
+                $view->parseError = true;
+
+                // We need to create and process an "empty results" object to
+                // ensure that recommendation modules and templates behave
+                // properly when displaying the error message.
+                $view->results = $this->getResultsManager()->get('EmptySet');
+                $view->results->setParams($params);
+                $view->results->performAndProcessSearch();
+            } else {
+                throw $e;
+            }
+        }
+        // Save statistics:
+        if ($this->logStatistics) {
+            $this->getServiceLocator()->get('VuFind\SearchStats')
+                ->log($results, $this->getRequest());
+        }
+
+        // Special case: If we're in RSS view, we need to render differently:
+        if (isset($view->results)
+            && $view->results->getParams()->getView() == 'rss'
+        ) {
+            $response = $this->getResponse();
+            $response->getHeaders()->addHeaderLine('Content-type', 'text/xml');
+            $feed = $this->getViewRenderer()->plugin('resultfeed');
+            $response->setContent($feed($view->results)->export('rss'));
+            return $response;
+        }
+
+        // Search toolbar
+        $config = $this->getServiceLocator()->get('VuFind\Config')->get('config');
+        $view->showBulkOptions = isset($config->Site->showBulkOptions)
+          && $config->Site->showBulkOptions;
+
+        return $view;
+    }
+
+    /**
+     * Process the jumpto parameter -- either redirect to a specific record and
+     * return view model, or ignore the parameter and return false.
+     *
+     * @param \VuFind\Search\Base\Results $results Search results object.
+     *
+     * @return bool|\Zend\View\Model\ViewModel
+     */
+    protected function processJumpTo($results, $jumpto = null)
+    {
+        if ($jumpto === null) {
+            // Missing/invalid parameter?  Ignore it:
+            $jumpto = $this->params()->fromQuery('jumpto');
+            if (empty($jumpto) || !is_numeric($jumpto)) {
+                return false;
+            }
+        }
+
+        // Parameter out of range?  Ignore it:
+        $recordList = $results->getResults();
+        if (!isset($recordList[$jumpto - 1])) {
+            return false;
+        }
+
+        // If we got this far, we have a valid parameter so we should redirect
+        // and report success:
+        $details = $this->getRecordRouter()
+            ->getTabRouteDetails($recordList[$jumpto - 1]);
+        return $this->redirect()->toRoute($details['route'], $details['params']);
     }
 }
