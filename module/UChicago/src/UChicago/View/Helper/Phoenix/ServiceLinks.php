5888fa3a19512c2ebf0a6ed8ba4a7c33d801d532
Brad Busenius <bbusenius@uchicago.edu>
2017-09-18 17:06:46 -0500

    Changed the icon for mansueto link.

--- module/UChicago/src/UChicago/View/Helper/Phoenix/ServiceLinks.php
+++ module/UChicago/src/UChicago/View/Helper/Phoenix/ServiceLinks.php
@@ -438,7 +438,7 @@ class ServiceLinks extends AbstractHelper {
     public function asr($row) {
         $defaultUrl = '/vufind/MyResearch/Storagerequest?bib=' .  $row['id'] . '&amp;barcode=' . $row['barcode'] . '&amp;action=add';
         $serviceLink = $this->getLinkConfig('mansueto', $defaultUrl); 
-        $displayText = '<i class="fa fa-fw fa-shopping-cart" aria-hidden="true"></i> Request from Mansueto Library';
+        $displayText = '<i class="fa fa-fw fa-shopping-basket" aria-hidden="true"></i> Request from Mansueto Library';
         $blacklist = array_map('strtolower', $this->lookupLocation['scrcInMansueto']);
         if (($serviceLink) and (in_array($row['status'], $this->lookupStatus['asr']) and $this->getLocation($row['locationCodes'], 'library') != 'spcl') and 
             (!in_array(strtolower($this->getLocation($row['locationCodes'], 'shelving')), $blacklist))) {
