f1214a5869659ccc56a72838f88e532fe6b91499
Brad Busenius <bbusenius@uchicago.edu>
2024-04-22 15:06:35 -0500

    Removed unused Zotero harvesting code.

--- themes/phoenix/theme.config.php
+++ themes/phoenix/theme.config.php
@@ -11,14 +11,12 @@ return [
             'UChicago\View\Helper\Phoenix\ServiceLinks' => 'UChicago\View\Helper\Phoenix\ServiceLinksFactory',
             'VuFind\View\Helper\Root\RecordDataFormatter' => 'UChicago\View\Helper\Phoenix\RecordDataFormatterFactory',
             'UChicago\View\Helper\Phoenix\RecordLinker' => 'VuFind\View\Helper\Root\RecordLinkerFactory',
-            'UChicago\View\Helper\Phoenix\ZoteroHarvesting' => 'UChicago\View\Helper\Phoenix\ZoteroHarvestingFactory',
         ],
         'aliases' => [
             'citation' => 'UChicago\View\Helper\Phoenix\Citation',
             'ServiceLinks' => 'UChicago\View\Helper\Phoenix\ServiceLinks',
             'recordDataFormatter' => 'VuFind\View\Helper\Root\RecordDataFormatter',
             'recordLinker' => 'UChicago\View\Helper\Phoenix\RecordLinker',
-            'ZoteroHarvesting' => 'UChicago\View\Helper\Phoenix\ZoteroHarvesting',
         ],
     ],
 ];
